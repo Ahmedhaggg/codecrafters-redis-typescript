c42e52c026fbd9926ad23872781231ca023d907f
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-10-15 01:21:50 +0300

    run test

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,20 @@
 import * as net from "net";
+
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
   connection.on("data", (data: Buffer) => {
-    connection.write("+PONG\r\n");
+    const [command, ...args] = data.toString().split(" ");
+
+    let commandSensitive = command.toUpperCase().trim();
+
+    if (commandSensitive === "PING") {
+      connection.write("+PONG\r\n");
+    } else if (commandSensitive === "ECHO") {
+      connection.write(`$${args[0].length}\r\n${args[0]}\r\n`);
+    } else {
+      connection.write("-ERR unknown command\r\n");
+    }
   });
 });
+
 server.listen(6379, "127.0.0.1");
