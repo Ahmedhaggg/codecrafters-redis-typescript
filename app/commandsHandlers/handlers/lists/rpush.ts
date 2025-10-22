3ef83af0e7bb0c4108ebe6d59d265cb1ca0492be
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-10-22 06:47:01 +0300

    complete push multiple  values to list

--- app/commandsHandlers/handlers/lists/rpush.ts
+++ app/commandsHandlers/handlers/lists/rpush.ts
@@ -1,24 +1,29 @@
 import { RespEncoder } from "../../../resp/encoder";
 import { RespBulkString, type RespCommand } from "../../../resp/objects";
 import { StoreManager } from "../../../store/store-manager";
+import { isContainsArgs } from "../../validation/contains-args.validator";
+import { isBulkStringArray } from "../../validation/isBulkStringList.validator";
 
 export const rpush = (command: RespCommand) => {
-  const listName = command.args?.[0];
-  const listValue = command.args?.[1];
-
-  if (listName instanceof RespBulkString == false || listValue instanceof RespBulkString == false) {
-    return RespEncoder.encodeError("Invalid key Or Value");
+  // Check that command has args
+  if (!isContainsArgs(command)) {
+    return RespEncoder.encodeError("Invalid key or value");
   }
 
-  let list = StoreManager.get().get(listName.value) ?? [];
+  const args = command.args;
 
-  if (!Array.isArray(list)) {
-    return RespEncoder.encodeError("key Not Reference To List");
+  // Narrow to RespBulkString[]
+  if (!isBulkStringArray(args)) {
+    return RespEncoder.encodeError("Invalid key or value");
   }
 
-  list.push(listValue.value);
+  const [listName, ...listValues] = args.map((a) => (a as RespBulkString).value);
 
-  StoreManager.get().set(listName.value, list);
+  let list = StoreManager.get().get(listName) ?? [];
+
+  list.push(...listValues);
+  console.log(list);
+  StoreManager.get().set(listName, list);
 
   return RespEncoder.encodeInteger(list.length);
 };
