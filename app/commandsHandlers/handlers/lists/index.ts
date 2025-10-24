30871b9a6b746eaf01e7938ad626b2b5cd455655
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-10-24 16:42:52 +0300

    fix return the array completely

--- app/commandsHandlers/handlers/lists/index.ts
+++ app/commandsHandlers/handlers/lists/index.ts
@@ -52,5 +52,5 @@ export const lRange = (command: RespCommand) => {
 
   const list = StoreManager.get().get(listName) ?? [];
 
-  return RespEncoder.encodeArray(list);
+  return RespEncoder.encodeArray(list.slice(startIndex, endIndex));
 };
