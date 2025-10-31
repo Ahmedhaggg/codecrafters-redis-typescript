9890c26a348585e95dc3d34acd40c11e9a24c39a
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-11-01 00:38:10 +0200

    prevent return response to master

--- app/commandsHandlers/index.ts
+++ app/commandsHandlers/index.ts
@@ -3,6 +3,7 @@ import { RespEncoder } from "../resp/encoder";
 import type { RespCommand } from "../resp/objects";
 import { handleTransaction, isTransactionCommand } from "./handlers/transactions/transactions-commands-handlers";
 import { getCommandHandler } from "./handlers.factory";
+import { config } from "../config/config";
 
 export const handleCommand = (command: RespCommand, connection: Socket) => {
   if (isTransactionCommand(command, connection)) return handleTransaction(command, connection);
@@ -11,11 +12,23 @@ export const handleCommand = (command: RespCommand, connection: Socket) => {
 
   if (handler.type == "OBSERVER") {
     const obsRes = handler.handler(command, connection);
-    if (obsRes) return connection.write(obsRes);
+    if (obsRes) {
+      if (config.replicaOf?.host == connection.remoteAddress && config.replicaOf?.port == connection.remotePort) {
+        return;
+      }
+      return connection.write(obsRes);
+    }
+
     return RespEncoder.encodeNil();
   }
 
-  if (handler.type == "REQ_RES") return connection.write(handler.handler(command));
+  if (handler.type == "REQ_RES") {
+    const res = handler.handler(command);
+    if (config.replicaOf?.host == connection.remoteAddress && config.replicaOf?.port == connection.remotePort) {
+      return;
+    }
+    return connection.write(res);
+  }
 
   connection.write(RespEncoder.encodeError("INVALID"));
 };
