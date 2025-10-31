74c23e1f54b90d207107bea1eb0885091a6b0483
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-10-31 10:19:28 +0200

    complete hadnshake

--- app/config/replica-config.ts
+++ app/config/replica-config.ts
@@ -6,17 +6,50 @@ export const connectReplicaToMaster = () => {
   const { host, port } = config.replicaOf!;
 
   console.log(" config.replicaOf ", config.replicaOf);
+
+  let currentHandShakeStep = 0;
+
+  const sendPing = () => {
+    client.write(RespEncoder.encodeArray([RespEncoder.encodeString("PING")]));
+  };
+
+  const sendListeningPort = () => {
+    client.write(
+      RespEncoder.encodeArray([
+        RespEncoder.encodeString("REPLCONF"),
+        RespEncoder.encodeString("listening-port"),
+        RespEncoder.encodeString(config.port.toString()),
+      ])
+    );
+  };
+
+  const sendCapa = () => {
+    client.write(
+      RespEncoder.encodeArray([
+        RespEncoder.encodeString("REPLCONF"),
+        RespEncoder.encodeString("capa"),
+        RespEncoder.encodeString("psync2"),
+      ])
+    );
+  };
+
   const client = net.createConnection(port, host, () => {
     console.log("Connected to master");
-    console.log(RespEncoder.encodeArray([RespEncoder.encodeString("PING")]));
-    const commands = [["PING"], ["REPLCONF", "listening-port", config.port.toString()], ["REPLCONF", "capa", "psync2"]];
-
-    commands.forEach((command) => {
-      client.write(RespEncoder.encodeArray(command.map((str) => RespEncoder.encodeString(str))));
-    });
+    sendPing();
+    currentHandShakeStep++;
   });
 
   client.on("data", (data) => {
-    console.log("Received: " + data);
+    console.log("data from master", data.toString());
+    if (currentHandShakeStep === 1) {
+      sendListeningPort();
+      currentHandShakeStep++;
+    } else if (currentHandShakeStep === 2) {
+      sendCapa();
+      currentHandShakeStep++;
+    }
   });
 };
+
+// console.log(RespEncoder.encodeArray([RespEncoder.encodeString("PING")]));
+// client.write(RespEncoder.encodeArray([RespEncoder.encodeString("PING")]));
