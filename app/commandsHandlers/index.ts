6b3f0904a08bfab73200c55f2f0516439ca8df32
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-11-01 00:43:02 +0200

    prevent return response to master

--- app/commandsHandlers/index.ts
+++ app/commandsHandlers/index.ts
@@ -12,21 +12,18 @@ export const handleCommand = (command: RespCommand, connection: Socket) => {
 
   if (handler.type == "OBSERVER") {
     const obsRes = handler.handler(command, connection);
-    if (obsRes) {
-      if (config.replicaOf?.host == connection.remoteAddress && config.replicaOf?.port == connection.remotePort) {
-        return;
-      }
-      return connection.write(obsRes);
-    }
+    if (config.replicaOf?.host == connection.remoteAddress && config.replicaOf?.port == connection.remotePort) return;
+
+    if (obsRes) return connection.write(obsRes);
 
     return RespEncoder.encodeNil();
   }
 
   if (handler.type == "REQ_RES") {
     const res = handler.handler(command);
-    if (config.replicaOf?.host == connection.remoteAddress && config.replicaOf?.port == connection.remotePort) {
-      return;
-    }
+
+    if (config.replicaOf?.host == connection.remoteAddress && config.replicaOf?.port == connection.remotePort) return;
+
     return connection.write(res);
   }
 
