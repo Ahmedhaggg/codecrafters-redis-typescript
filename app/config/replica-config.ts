c3a788a69d59f7fb4fbc81d69936360ec58abbc5
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-11-04 02:15:31 +0200

    fix

--- app/config/replica-config.ts
+++ app/config/replica-config.ts
@@ -1,9 +1,6 @@
 import * as net from "net";
 import { config } from "./config";
 import { RespEncoder } from "../resp/encoder";
-import { RespDecoder } from "../resp/decoder";
-import { RespCommand } from "../resp/objects";
-import { handleCommand } from "../commandsHandlers";
 
 export const connectReplicaToMaster = () => {
   const { host, port } = config.replicaOf!;
@@ -64,41 +61,6 @@ export const connectReplicaToMaster = () => {
       currentHandShakeStep++;
     } else {
       console.log("data from master", data.toString());
-      const str = data.toString();
-      console.log("replica received ", str);
-      const commands = str
-        .split(/\*(?=\d+)/)
-        .filter(Boolean)
-        .map((chunk) => "*" + chunk.trim());
-
-      for (const cmdStr of commands) {
-        console.log("cmdStr: ", cmdStr);
-
-        const cmdBuffer = Buffer.from(cmdStr);
-        if (!cmdStr.startsWith("*")) {
-          console.log("is not set command ", cmdStr);
-          continue;
-        }
-
-        try {
-          const respDecoder = new RespDecoder(cmdBuffer);
-          const command = respDecoder.decode();
-
-          // Must be a valid RespCommand
-          if (!(command instanceof RespCommand)) continue;
-
-          // Only process SET commands
-          if (command.command?.toUpperCase() !== "SET") continue;
-
-          console.log("SET command received:", command.args);
-
-          // Handle or store the SET command
-          handleCommand(command, client);
-        } catch {
-          // Ignore invalid / partial RESP data (like binary RDB dump)
-          continue;
-        }
-      }
     }
   });
 };
