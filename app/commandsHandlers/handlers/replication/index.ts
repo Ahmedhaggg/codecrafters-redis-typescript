34ca8309b6df7e8cf5aed63b7c0cad35b711af15
Ahmedhaggg <ahmedhaggagrady@gmail.com>
2025-11-06 08:22:12 +0200

    comp

--- app/commandsHandlers/handlers/replication/index.ts
+++ app/commandsHandlers/handlers/replication/index.ts
@@ -53,7 +53,7 @@ export const replconf = (command: RespCommand) => {
 
   if (firstArg == "ACK" && waitCommand.isPending) {
     waitCommand.syncedReplicasCount++;
-    return RespEncoder.encodeSimpleString("OK");
+    return;
   }
 
   return RespEncoder.encodeError("ERR unknown REPLCONF option");
